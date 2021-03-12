19c28cd9cfcd9f8324d0247ee63e284cdbc3a84c
Cy-real <79482914+Cy-real@users.noreply.github.com>
2021-03-12 21:03:08 +0300

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,12 +1,10 @@
 module.exports = function towelSort (matrix) {
   let arr = [];
-    let counter =0;
     for(let i=0;i<matrix[i].length;i++)
     {
         for(let j=0;j<matrix[j].length;j++)
         {
-            arr[counter] = matrix[i][j];
-            counter++;
+            arr.push(matrix[i][j]);
         }
     }
     
